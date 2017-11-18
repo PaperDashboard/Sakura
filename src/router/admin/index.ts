3a1e3a6adcb47d2463eddad4148079f39028b58f
Indexyz <jiduye@gmail.com>
2017-11-19 01:41:47 +0800

    fix(permission): normal user can't access admin api

--- src/router/admin/index.ts
+++ src/router/admin/index.ts
@@ -4,6 +4,21 @@ import * as node from './node'
 
 const router: Router = R()
 
+router.use((req: any, res, next) => {
+    if (req.method === "OPTIONS") {
+        return next()
+    }
+
+    if (!req.user.isAdmin) {
+        return res.status(403).send({
+            status: 'error',
+            message: 'User no permission'
+        })
+    } else {
+        next()
+    }
+})
+
 router.use('/node', node.default)
 
 export default router
