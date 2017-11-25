01fa8e0f1150843aa66021c875e5396a92994cc0
Indexyz <jiduye@gmail.com>
2017-11-25 22:25:02 +0800

    ref(redis): change redis setting from host to url

--- src/middleware/redis.ts
+++ src/middleware/redis.ts
@@ -5,11 +5,7 @@ import * as redis from 'ioredis'
 function loadRedis(req, res, next) {
     const config: IConfig = req.config
 
-    let client = new redis(config.get("redis.port"), config.get("redis.host"))
-
-    if (config.get("redis.auth") && config.get("redis.auth") != "REDIS_AUTH") {
-        client.auth(config.get("redis.auth"))
-    }
+    let client = new redis(config.get("redis.url"))
 
     Object.assign(req, {
         db: {
