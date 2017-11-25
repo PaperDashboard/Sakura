a7bca6f7609a5c94902b39e782a7bc31d76f9859
Indexyz <jiduye@gmail.com>
2017-11-25 23:04:44 +0800

    fix(redis): Try fix too many clients

--- src/middleware/redis.ts
+++ src/middleware/redis.ts
@@ -1,15 +1,22 @@
 import { IConfig } from "config";
 import * as util from 'util'
 import * as redis from 'ioredis'
+import config from '../config'
+import { Redis } from "ioredis";
+
+class RedisServer{
+    public static redis: Redis
+    constructor() {
+        RedisServer.redis = new redis(config.get("redis.url"))
+    }
+}
+
+new RedisServer()
 
 function loadRedis(req, res, next) {
-    const config: IConfig = req.config
-
-    let client = new redis(config.get("redis.url"))
-
     Object.assign(req, {
         db: {
-            redis: client
+            redis: RedisServer.redis
         }
     })
     next()
