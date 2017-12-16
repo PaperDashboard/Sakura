c74291b81572f9176f2f8995877ca1e49a2248bb
Indexyz <jiduye@gmail.com>
2017-12-17 02:28:15 +0800

    fix(user/nodeList): fix async error

--- src/router/dashboard/node/list.ts
+++ src/router/dashboard/node/list.ts
@@ -1,7 +1,7 @@
 export default {
     async get(req, res, next) {
         const user = req.user;
-        const nodes = req.service.node.getUserAvailAble(user._id);
+        const nodes = await req.service.node.getUserAvailAble(user._id);
         res.status(200).send({
             status: 'success',
             nodes
